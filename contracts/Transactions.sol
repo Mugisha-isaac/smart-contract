289d4de1665e99692f513c8de20ea863f9f90429
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-08 22:24:47 +0200

    [wip: Transactions.sol] created event Transfer

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -1,3 +1,9 @@
 //  SPDX-License-Identifier:  UNLICENSED
 
-paragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
+
+contract Transactions{
+   uint256  TransactionCounter;
+
+   event  Transfer(address from, address receiver, uint amount, string message, unint256 timestamp, string keyword);
+}
