e7ead6600fe8a8243f06271f5e86f895998ffe50
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-08 22:27:41 +0200

    [feat: Transactions.sol] defining Transfer structure

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -6,4 +6,13 @@ contract Transactions{
    uint256  TransactionCounter;
 
    event  Transfer(address from, address receiver, uint amount, string message, unint256 timestamp, string keyword);
+
+   struct TransferStruct{
+      address sender;
+      address receiver;
+      uint amount;
+      string message; 
+      uint256 timestamp;
+      string keyword;
+   }
 }
